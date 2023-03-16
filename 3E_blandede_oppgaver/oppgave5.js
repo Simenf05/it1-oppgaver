543a59e7d9f32b549c0b199555d273cd768393b7
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-03-16 16:42:28 +0100

    oppgave5 ferdig

--- 3E_blandede_oppgaver/oppgave5.js
+++ 3E_blandede_oppgaver/oppgave5.js
@@ -6,7 +6,31 @@ const helTall = endNum => [...Array(endNum).keys()].map(x => x + 1)
 const arr10Heltall = helTall(10)
 
 
+const randomIntInRange = max => Math.floor(Math.random() * max)
+
+function trekkTall(arr, antallTekking) {
+
+    if (antallTekking > arr.length) throw Error("ei du kan ikke do that")
+
+    const origin = [...arr]
+    const trekkesFra = [...arr]
+    const trukket = []
+    
+    for (let i = 0; i < antallTekking; i++) {
+        
+        const index = randomIntInRange(trekkesFra.length)
+        trukket.push(trekkesFra[index])
+        trekkesFra.splice(index, 1)
+        
+    }
+
+    return {
+        origin: origin,
+        leftOver: trekkesFra,
+        trukket: trukket,
+    }
+}
 
 
-
-
+const val = trekkTall(arr10Heltall, -5)
+console.log(val)
